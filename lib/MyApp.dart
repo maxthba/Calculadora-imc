a41c822a8da162c6ea208c8ce366eafe19d8a029
Max <maxthomaz2006@gmail.com>
2026-03-16 18:03:54 -0300

    commitando separacao de classes por aquivos diferentes

---
+++ lib/MyApp.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'MyHomePage.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
---
+++ lib/MyHomePage.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import '_MyHomePageState.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
