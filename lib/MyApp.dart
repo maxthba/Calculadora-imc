91c5d8f518e4085008f2a65261ba81dfd0fbe895
Max <maxthomaz2006@gmail.com>
2026-03-16 18:30:56 -0300

    commitando parte de myapp

--- lib/MyApp.dart
+++ lib/MyApp.dart
@@ -1,19 +1,23 @@
 import 'package:flutter/material.dart';
 import 'MyHomePage.dart';
 
+// Widget raiz do aplicativo.
+// Aqui definimos configurações globais, como tema e tela inicial.
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      // Nome do app utilizado pelo sistema operacional e por algumas integrações.
+      title: 'Calculadora IMC',
       theme: ThemeData(
-
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        // Gera uma paleta de cores baseada em uma cor semente.
+        // Isso mantém um visual consistente em todo o app.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      // Define a tela principal única do aplicativo.
+      home: const MyHomePage(title: 'Calculadora de IMC'),
     );
   }
 }
